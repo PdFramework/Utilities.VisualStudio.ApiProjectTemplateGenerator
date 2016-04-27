80050ec6e644a99691139d0746acae5e9b2ab03d
peinearydevelopment <peinearydevelopment@gmail.com>
2016-04-27 15:09:36 -0400

    Created 'DataAccess' project.

---
+++ Projects/DataAccess/BusinessComponentsDal.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using PeinearyDevelopment.Framework.ApiBases.Data.Entity;
+
+namespace PeinearyDevelopment.Utilities.VisualStudio.ApiProjectTemplateGenerator.Projects.DataAccess
+{
+  public class BusinessComponentsDal : DbContextBase<BusinessComponentDto>, IBusinessComponentsDal
+  {
+    public BusinessComponentsDal(DbContext businessComponentsDbContext) : base(businessComponentsDbContext)
+    {
+    }
+    public async Task<BusinessComponentDto> Update(BusinessComponentDto businessComponentDto)
+    {
+      var storedBusinessComponentDto = await Read(businessComponentDto.Id);
+      storedBusinessComponentDto.StartDateTimeOffset = businessComponentDto.StartDateTimeOffset;
+      storedBusinessComponentDto.StopDateTimeOffset = businessComponentDto.StopDateTimeOffset;
+      await DbContext.SaveChangesAsync();
+      return storedBusinessComponentDto;
+    }
+  }
+}
---
+++ Projects/DataAccess/BusinessComponentsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace PeinearyDevelopment.Utilities.VisualStudio.ApiProjectTemplateGenerator.Projects.DataAccess
+{
+  public class BusinessComponentsDbContext : DbContext
+  {
+    public BusinessComponentsDbContext(): base("BusinessComponentsDbContext")
+    {
+    }
+
+    public virtual DbSet<BusinessComponentDto> BusinessComponents { get; set; }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<BusinessComponentDto>()
+                  .ToTable("BusinessComponents", "dbo");
+    }
+  }
+}
