7e5b89e5abf0f405adee2d35ee39f79e868d4115
peinearydevelopment <peinearydevelopment@gmail.com>
2016-04-27 16:43:26 -0400

    Added 'DataAccess' template

---
+++ Templates/DataAccess/$specificconcept$DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace $customnamespace$.DataAccess
+{
+  public class $specificconcept$DbContext : DbContext
+  {
+    public $specificconcept$DbContext(): base("$specificconcept$DbContext")
+    {
+    }
+
+    public virtual DbSet<$specificconceptsingularized$Dto> $specificconcept$ { get; set; }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<$specificconceptsingularized$Dto>()
+                  .ToTable("$specificconcept$", "dbo");
+    }
+  }
+}
---
+++ Templates/DataAccess/$specificconceptsingularized$Dto.cs
@@ -0,0 +1,8 @@
+﻿using PeinearyDevelopment.Framework.ApiBases.DataAccess;
+
+namespace $customnamespace$.DataAccess
+{
+    public class $specificconceptsingularized$Dto : DateRangeValidDtoBase
+    {
+    }
+}
