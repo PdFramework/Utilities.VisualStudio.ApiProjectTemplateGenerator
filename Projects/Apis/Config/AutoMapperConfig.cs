e1873fba7f751435b3a3c57088b612cbbcdc535b
peinearydevelopment <peinearydevelopment@gmail.com>
2016-04-27 15:26:04 -0400

    Added 'Apis' project.

---
+++ Projects/Apis/Config/AutoMapperConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using PeinearyDevelopment.Utilities.VisualStudio.ApiProjectTemplateGenerator.Projects.Contracts;
+using PeinearyDevelopment.Utilities.VisualStudio.ApiProjectTemplateGenerator.Projects.DataAccess;
+
+namespace PeinearyDevelopment.Utilities.VisualStudio.ApiProjectTemplateGenerator.Projects.Apis
+{
+  public class AutoMapperConfig
+  {
+    public static IMapper Mapper { get; protected set; }
+
+    public static void Configure()
+    {
+      var config = new MapperConfiguration(cfg =>
+      {
+        cfg.CreateMap<BusinessComponentDto, BusinessComponent>();
+        cfg.CreateMap<BusinessComponent, BusinessComponentDto>();
+      });
+
+      Mapper = config.CreateMapper();
+    }
+  }
+}
---
+++ Projects/Apis/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PeinearyDevelopment.Utilities.VisualStudio.ApiProjectTemplateGenerator.Projects.Apis.WebApiApplication" Language="C#" %>
