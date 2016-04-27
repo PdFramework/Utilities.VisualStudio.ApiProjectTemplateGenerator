d323bccd88c240d540dc8e990044833631d9a8ca
peinearydevelopment <peinearydevelopment@gmail.com>
2016-04-27 15:05:04 -0400

    Created 'Contracts' project.

---
+++ Projects/Contracts/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace PeinearyDevelopment.Utilities.VisualStudio.ApiProjectTemplateGenerator.Projects.Contracts
+{
+    public static class Constants
+    {
+      public const string BusinessComponentsEndpointKey = "BusinessComponentsBaseUrl";
+      public const string BaseRoutePrefix = "v1/BusinessComponents";
+      public const string BaseBusinessComponentsRoutePrefix = BaseRoutePrefix + "/BusinessComponents";
+    }
+}
