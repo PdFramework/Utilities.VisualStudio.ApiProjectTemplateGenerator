fd977a7970f8e10f7a47760cfb8f727e4f1c2115
peinearydevelopment <peinearydevelopment@gmail.com>
2016-04-28 14:20:40 -0400

    Created a project template generator PowerShell script.

--- Templates/DataAccess/I$specificconcept$Dal.cs
+++
@@ -1,8 +0,0 @@
-﻿using PeinearyDevelopment.Framework.ApiBases.DataAccess;
-
-namespace $customnamespace$.DataAccess
-{
-  public interface I$specificconcept$Dal : IDbContextBase<$specificconceptsingularized$Dto>
-  {
-  }
-}
